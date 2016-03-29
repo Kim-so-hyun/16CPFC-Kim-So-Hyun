bb73ecf8721bb734b776123d8267403a58f8f9d5
CAD Client <CAD Client>
2016-03-29 13:03:57 +0900

    ex06.1

--- ex06/ex06.py
+++ ex06/ex06.py
@@ -1,4 +1,4 @@
-x = "There are %d types of people." % 10
+x = "There are %d types of people" % 10
 binary = "binary"
 do_not = "don't"
 y = "Those who know %s and those who %s." % (binary, do_not)
@@ -11,6 +11,7 @@ print "I also said: '%s'." % y
 
 hilarious = False
 joke_evaluation = "Isn't that joke so funny?! %r"
+
 
 print joke_evaluation % hilarious
 
