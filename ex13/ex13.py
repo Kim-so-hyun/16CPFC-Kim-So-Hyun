6f87f38afb4f6ecfd657e9dd2a26054dbf16810f
Kim-So-Hyun <rlath56@naver.com>
2016-04-26 06:28:12 +0900

    ex13

--- ex13/ex13.py
+++ ex13/ex13.py
@@ -1,8 +1,8 @@
 from sys import argv
 
-script, first, second, thirgd = argv
+script, first, second, third = argv
 
 print "The script is called:", script
 print "Your first variable is:", first
 print "Your second variable is:", second
-print "Your third variable is:", thirgd
+print "Your third variable is:", third
