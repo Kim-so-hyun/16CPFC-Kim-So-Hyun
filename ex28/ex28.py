6d61d99b9efb0ec3572c9422177182ca06959cde
CAD Client <CAD Client>
2016-06-07 09:47:33 +0900

    added

---
+++ ex28/ex28.py
@@ -0,0 +1,5 @@
+print (" 1. True and True = %s" % str(True and True))
+print (" 2. False and True = %s" % str(False and True))
+print (" 3. 1==1 and 2==1 = %s" % ((1==1) and (2==1)))
+print (" 4. 'test' == 'test' = %s" % str('test' == 'test'))
+print ()
